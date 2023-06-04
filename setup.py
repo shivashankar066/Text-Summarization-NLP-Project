e877c4282430e48d9fd3f53f725bfaff76e595ea
Shivashankar <shivashankar066@gmail.com>
2023-06-04 20:05:05 +0530

    Changes in setup

--- setup.py
+++ setup.py
@@ -7,11 +7,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME = "entbappy"
+AUTHOR_USER_NAME = "shivashankar066"
 SRC_REPO = "textSummarizer"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
-
-
+AUTHOR_EMAIL = "shivashankar066@gmail.com"
 
 setuptools.setup(
     name=SRC_REPO,
--- template.py
+++ template.py
@@ -10,7 +10,7 @@ project_name = "textSummarizer"
 list_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
-    f"src/{project_name}/conponents/__init__.py",
+    f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/utils/common.py",
     f"src/{project_name}/logging/__init__.py",
@@ -39,14 +39,10 @@ for filepath in list_of_files:
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory:{filedir} for the file {filename}")
 
-    
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath,'w') as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
-
-
-    
     else:
         logging.info(f"{filename} is already exists")
 
